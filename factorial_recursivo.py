0af57f52b1400fef9d98992bbda6161930c419e2
GioSENPAII <89473339+GioSENPAII@users.noreply.github.com>
2023-04-24 19:05:33 -0600

    Update factorial_recursivo.py

--- factorial_recursivo.py
+++ factorial_recursivo.py
@@ -6,6 +6,6 @@ def factorial_recursivo(n):
 
 print(factorial_recursivo(5))
 
-#De acuerdo al teorema del Maestro este algoritmo tiene una complejidad de O(logn) ya que primero "a" tiene una cantidad de llamados recursivos de 1, después n/b es igual a n
-#por lo tanto b=1 y finalmente tenemos que O(1) ya que las operaciones son en tiempo constante, por lo tanto c=0
+#De acuerdo al teorema del Maestro este algoritmo tiene una complejidad de O(logn) ya que primero "a" tiene una cantidad de llamados recursivos de 1, después n/b es igual a n-1
+#por lo tanto b=5/4 y finalmente tenemos que O(1) ya que las operaciones son en tiempo constante, por lo tanto c=0
 #Finalmente sustituyendo tenemos que O(logn).
